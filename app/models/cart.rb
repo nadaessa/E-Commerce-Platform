a23dbf97b1be7e3247d5c83a8a6f011fbe2821c7
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-04-28 03:20:45 +0200

    cart-cartitem ->migration

---
+++ app/models/cart.rb
@@ -0,0 +1,3 @@
+class Cart < ApplicationRecord
+    belongs_to :user, foreign_key: "user_id" 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_002716) do
+ActiveRecord::Schema.define(version: 2019_04_28_010445) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity"
+  end
+
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
