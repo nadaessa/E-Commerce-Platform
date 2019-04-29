63d48a85e755785d36a6f8ef08bbb2e3941202db
nadaessa <nessam378@gmail.com>
2019-04-29 18:24:46 +0200

    initial crud product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
 
+    def show
+        @product = Product.find(params[:id])
+    end
+    
     def new
         @product = Product.new
+    end
+
+    def edit
+        @article = Product.find(params[:id])
     end
        
     def create
@@ -13,10 +24,27 @@ class ProductsController < ApplicationController
           render 'new'
         end
     end
+
+    def update
+        @product = Product.find(params[:id])
+     
+        if @product.update(product_params)
+          redirect_to @product
+        else
+          render 'edit'
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+     
+        redirect_to products_path
+    end
        
     private
         def product_params
-          params.require(:product).permit(:title, :text)
+          params.require(:product).permit(:title, :description )
         end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_091908) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "price"
+    t.float "price"
     t.integer "quantity"
     t.string "image", default: "null"
     t.datetime "created_at", null: false
