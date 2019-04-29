b3b27612c6ce6dceb444c0917520f85a2fcaac9d
nadaessa <nessam378@gmail.com>
2019-04-29 12:39:58 +0200

    generate home controller and index function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'form/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
