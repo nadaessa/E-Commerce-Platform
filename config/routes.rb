e71c5a932e7d3128698e4c59cccc5d3fcfe1785d
sarahmohamed22 <sarahmohamed@alexu.edu.eg>
2019-04-30 14:16:24 +0200

    create brands

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'form/new'
+ 
+  resources :brands
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
